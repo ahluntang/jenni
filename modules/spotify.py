fcc535e6ca9b708dbcb48fa4324f869f706a766b
Ah-Lun Tang <tang@ahlun.be>
2014-04-28 23:58:02 +0200

    Update spotify.py

--- modules/spotify.py
+++ modules/spotify.py
@@ -150,5 +150,5 @@ def query(jenni, input):
     except KeyError:
         notify(jenni, input.nick, "Unknown response from API server")
 
-query.rule = r'.*spotify:(.*?)\)$'
+query.rule = r'.*spotify:(.*?)[)]*$'
 query.priority = 'low'
